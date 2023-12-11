edbbebf167b92e81f1928968acfc03464ba4fb60
Doryush Normatov <normatov_d@auca.kg>
2023-12-11 16:38:58 +0600

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
 <body>  
     <section class="headerOfWebSite"> 
         <nav>
-            <a href="index.html"><img src="images/Mountain-logo-template-on-transparent-background-PNG.png"></a>
+            <a href="index.html"><img src="https://github.com/normatov-d-auca-2021/AUCAKIT2023/blob/master/src/routes/images/Mountain-logo-template-on-transparent-background-PNG.png"></a>
             <div class="navigationLinks" id="navLinks">
                 <i class="fa fa-times" onclick="hideMenu() "></i>
                     <ul>
